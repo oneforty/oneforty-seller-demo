fd5476ecbe81d1c0f77543be69241506261ae901
Robby Grossman <robby@freerobby.com>
2009-11-23 17:32:59 -0500

    Create 404 and 500 responses

--- oneforty-seller-demo.rb
+++ oneforty-seller-demo.rb
@@ -4,3 +4,11 @@ require 'sinatra'
 get '/' do
   "Hello from the oneforty demo store!"
 end
+
+not_found do
+  "The page you're looking for is not part of the oneforty seller demo."
+end
+
+error do
+  "Something went wrong. If you have questions about how to use this demo app, please let us know at developers@oneforty.com"
+end
