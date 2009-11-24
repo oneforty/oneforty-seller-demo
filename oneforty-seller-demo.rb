1ab67f2b6f863e3efd43207519d1cd15dad1e815
Michael Macasek <michael@oneforty.com>
2009-11-24 14:43:56 -0500

    added comments for todos on successful fulfillment

--- oneforty-seller-demo.rb
+++ oneforty-seller-demo.rb
@@ -5,23 +5,16 @@ get '/' do
   "Hello from the oneforty demo store!"
 end
 
-# Process an ORDER ACKNOWLEDGMENT from oneforty.
-# A POST to this URL from oneforty alerts you that you have just made a sale.
-# You should respond with status 200 if you are ready to process it, or status 500 if your processing system is down.
-# If you respond with status 500, we will keep retrying every so often until we get a status 200.
-post '/order-acknowledgment' do
+post '/sale_notification' do
   begin
     status 200 # Tell oneforty that you're ready to process the order!
     "success!"
-    # You now need to ping us back over SSL so that we can exchange sensitive data.
+
+    # TODO need to set timeout for do_successful_fulfillment to execute.
   rescue e
     status 500 # Tell oneforty that something went wrong. We'll keep hitting you every so often until we get a 200.
     "failure :-("
   end
-end
-
-get '/order-acknowledgment' do
-  "Sorry, GET requests are not supported for order acknowledgments!"
 end
 
 not_found do
@@ -30,4 +23,13 @@ end
 
 error do
   "Something went wrong. If you have questions about how to use this demo app, please let us know at developers@oneforty.com"
+end
+
+def do_successful_fulfillment
+  # hit /fulfillment/acknowledgment via ssl with dev key and tranasaction #
+  #       recieve order info in response
+  # generate key info
+  # hit /fulfillment/complete url via ssl with dev key, trans #, and license info
+  #       recieve success in response
+  # done
 end
